adbb33481a5fbd737e5311df39c2ce1eab4029e1
parkyoula <youlapark@naddle.net>
2024-10-23 13:50:41 +0900

    회원사진 추가

--- dtos/Cellphone.php
+++ dtos/Cellphone.php
@@ -28,17 +28,24 @@ class Cellphone
     private ?int $_member_id;
 
     /**
+     * @var string $_photo 회원사진
+     */
+    private ?string $_photo;
+
+    /**
      * 휴대전화번호 구조체를 정의한다.
      *
      * @param string $cellphone 휴대전화번호
      * @param ?string $name 이름
      * @param ?int $member_id 회원고유값
+     * @param ?string $photo 회원사진
      */
-    public function __construct(string $cellphone, ?string $name = null, ?int $member_id = null)
+    public function __construct(string $cellphone, ?string $name = null, ?int $member_id = null, ?string $photo = null)
     {
         $this->_cellphone = $cellphone;
         $this->_name = $name;
         $this->_member_id = $member_id;
+        $this->_photo = $photo;
     }
 
     /**
@@ -76,6 +83,20 @@ class Cellphone
     }
 
     /**
+     * 회원사진을 가져온다.
+     *
+     * @return string $_photo
+     */
+    public function getPhoto(): string
+    {
+        /**
+         * @var \modules\member\Member $mMember
+         */
+        $mMember = \Modules::get('member');
+        return $mMember->getMemberPhoto($this->_member_id);
+    }
+
+    /**
      * JSON 으로 변환한다.
      *
      * @return object $json
@@ -86,6 +107,7 @@ class Cellphone
         $address->cellphone = $this->_cellphone;
         $address->name = $this->_name;
         $address->member_id = $this->_member_id;
+        $address->photo = $this->_photo;
 
         return $address;
     }
